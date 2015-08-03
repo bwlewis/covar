4dcc325a41e3b9a18a82672abaf77f4e227402fb
B. W. Lewis <blewis@illposed.net>
2015-08-03 10:02:00 -0400

    Removed absolute value in squared terms where appropriate

--- threshold/experiments.R
+++ threshold/experiments.R
@@ -14,16 +14,29 @@ C[lower.tri(C,diag=TRUE)] = 0  # cut out symmetric part and ones on diagonal
 x2  = tcor(A,t,p=2)
 x4  = tcor(A,t,p=4)
 x8  = tcor(A,t,p=8)
-x20 = tcor(A,t,p=20)
 x24 = tcor(A,t,p=24)
-x32 = tcor(A,t,p=32)
 
 d = function(i) diag(s$v[,1:i] %*% diag(s$d[1:i]^2) %*% t(s$v[,1:i]))
 
 
 
-par(mfrow=c(2,2))
+p = par(mfrow=c(2,2))
 plot(x2$dnum,ylab="",xlab="index difference",main=paste("2-d projection, num. candidates =",nrow(x2$candidates)))
 plot(x4$dnum,ylab="",xlab="index difference",main=paste("4-d projection, num. candidates =",nrow(x4$candidates)))
 plot(x8$dnum,ylab="",xlab="index difference",main=paste("8-d projection, num. candidates =",nrow(x8$candidates)))
 plot(x24$dnum,ylab="",xlab="index difference",main=paste("24-d projection, num. candidates =",nrow(x24$candidates)))
+par(p)
+
+
+# Shows how conservative the sqrt(2*(1-t))/d[1] bound is
+#A = matrix(rnorm(100*500),nrow=100)
+#C = cor(A)
+#C[lower.tri(C,diag=TRUE)] = 0  # cut out symmetric part and ones on diagonal
+#t = 0.80*max(C)
+#i = which(C>=t,arr.ind=TRUE)
+#A = scale(A,center=TRUE,scale=sqrt(apply(A,2,crossprod)))
+#s = svd(A)
+#lim = sqrt(2*(1-t))/s$d[1]
+#x = abs(s$v[i[,1],1] - s$v[i[,2],1])
+#plot(x, ylim=c(0,lim*1.2))
+#abline(h=lim,col=2)
