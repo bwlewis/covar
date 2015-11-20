1cfd54ef9690f4b1a0894bc0a5cc89bec1f6870d
bwlewis <blewis@illposed.net>
2015-11-20 13:39:45 -0500

    Updated example tcor.R implementation

--- threshold/example.R
+++ threshold/example.R
@@ -1,7 +1,6 @@
 # EXAMPLE USE of tcor
 source("tcor.R")
 
-
 library(biclust)  # for the EisenYeast and BicatYeast data
 data(BicatYeast)  # in gene by sample orientation (a tiny example for testing)
 A = t(BicatYeast) # to correlate genes to genes
@@ -14,32 +13,5 @@ t = 0.95
 x = tcor(A, t=t, p=10)
 
 # Compare
-# First note, there are 115 pairs with better than 0.95 correlation:
-sum(C>t)
-
-# and our algorithm with p=10 returns a list of 431 possible candidates:
+sum(C > t)
 dim(x)
-
-# We need to verify that x at least contains the 115 good pairs! Here is
-# one cheesy way to tell. First note that the entries of x are in somewhat
-# arbitrary order defined by the first singular vector. For example, the
-# pair (15,3) might be listed but not the equivalent redundant pair (3,15).
-# We account for that and build the following list of candidate
-# pairs in order so that the first coordinate is less than the second:
-check = function(x,C)
-{
-x = x[[1]]
-i = which(x[,2] < x[,1])
-x[i,] = x[i,2:1]
-# Express the coordinates as strings
-ans = apply(x,1,paste,collapse=",")
-
-# Now the actual list of correlated pairs as strings (already ordered):
-actual = apply(which(C>t, arr.ind=TRUE),1,paste,collapse=",")
-
-# Now check that all the actual correlated pairs are accounted for in the
-# candidates:
-print(length(actual))
-print(length(intersect(actual, ans)))
-# It's 115, so all the actual correlated pairs were found (plus some extra)!
-}
--- threshold/tcor.R
+++ threshold/tcor.R
@@ -1,10 +1,5 @@
-# EXPERIMENTAL PROTOTYPE FUNCTION
-# For an example use of this, see the file example.R.
-#
-# As of 20-July, 2015 this needs the development version of irlba available
-# from GitHub: devtools::install_github("bwlewis/IRL")
 require(irlba)
-
+require(parallel)
 #' Compute the thresholded correlations between columns of a matrix.
 #'
 #' Increase p to cut down the total number of candate pairs.
@@ -13,42 +8,31 @@ require(irlba)
 #' @param t a threshold value for correlation
 #' @param p projected subspace dimension
 #'
-#' @return
-#' A list with entries:
-#' candidates: A two-column matrix. Each row of the matrix are the
-#'             indices of candidate vectors that meet the correlation threshold.
-#' dnum: a vector of length equal to the longest run of adjacent points
-#'       in v_1 meeting the threshold that contains the number of candidate
-#'       entries making the threshold cut at each index difference.
-tcor = function(A, t=0.99, p=5, reduce=1)
+#' @return A two-column matrix. Each row of the matrix are the
+#' column indices of vectors that meet the correlation threshold.
+tcor = function(A, t=0.99, p=5)
 {
   mu = colMeans(A)
   s  = sqrt(apply(A,2,crossprod) - nrow(A)*mu^2) # column norms of centered matrix
   L  = irlba(A, p, center=mu, scale=s)
   P  = order(L$v[,1])  # order the entries of v1 (the permutation in the paper)
   limit = sqrt(2*(1-t))/L$d[1]
-# XXX EXPERIMENTAL reduce limit by fraction of variance in 1-d
-#if(reduce) limit = limit*L$d[1]^2/ncol(A) 
-limit = limit*reduce
-# sum of squared singular values of centered/scaled A = ncol(A)
 
 # XXX this is stupid, find a better way ('l' or LaTeX '\ell' in the paper):
   v = L$v[P,1]
   ell = max(vapply(1:length(v), function(i) {x=v[-(1:i)];sum(x-x[1] <= limit)}, 1))
 
-cat("longest run of adjacent points within threshold l =",ell,"\n")
-
-  dnum = rep(0,ell)
-# this is the big union in step 4 of algorithm 2.1, combined with step 6 to
-# convert back to original indices:
-  candidates = lapply(1:ell, function(i)
+# This is the big union in step 4 of algorithm 2.1, combined with step 6 to
+# convert back to original indices, and step 7 to evaluate the candiadtes.
+# Each step from 1 to \ell is independent of the others; the steps may be run
+# in parallel.
+  indices = mclapply(1:ell, function(i)
   {
     d = diff(L$v[P,1:p,drop=FALSE],lag=i)^2 %*% L$d[1:p]^2
     j = which(d <= 2*(1-t))  # These ordered indices meet the threshold
-    dnum[i] <<- length(j)    # Record the number of them
-    # return original un-permuted column indices
+    # return original un-permuted column indices that meet threshold (step 7)
+    if(length(j) > 0) j = j[vapply(j, function(k) cor(A[,P[k]], A[,P[k+i]]) >= t, TRUE)]
     cbind(P[j], P[j+i])
   })
-# Return candidates and dnum
-  list(candidates=Reduce(rbind,candidates), dnum=dnum)
+  Reduce(rbind,indices)
 }
