291d8ea7e60d4cc6c1199ad7d77f79f93a286264
B. W. Lewis <blewis@illposed.net>
2015-11-25 02:07:50 -0500

    reference implementation tweaks

--- threshold/example.R
+++ threshold/example.R
@@ -2,16 +2,24 @@
 source("tcor.R")
 
 library(biclust)  # for the EisenYeast and BicatYeast data
-data(BicatYeast)  # in gene by sample orientation (a tiny example for testing)
-A = t(BicatYeast) # to correlate genes to genes
+#data(BicatYeast)  # in gene by sample orientation (a tiny example for testing)
+#A = t(BicatYeast) # to correlate genes to genes
+data(EisenYeast)  # slightly larger example
+A = t(EisenYeast) # to correlate genes to genes
 
+t = 0.95
+
+t1 = proc.time()
 C = cor(A)                     # actual correlation matrix for reference
 C[lower.tri(C,diag=TRUE)] = 0  # cut out symmetric part and ones on diagonal
+C = C[C>t]
+print(proc.time() - t1)
 
+t1 = proc.time()
 # Compute candidate pairs with the new algorithm
-t = 0.95
 x = tcor(A, t=t, p=10)
+print(proc.time() - t1)
 
 # Compare
-sum(C > t)
-dim(x)
+print(sum(C > t))
+print(nrow(x$cor))
--- threshold/tcor.R
+++ threshold/tcor.R
@@ -1,5 +1,6 @@
 require(irlba)
-require(parallel)
+require(doParallel)
+
 #' Compute the thresholded correlations between columns of a matrix.
 #'
 #' Increase p to cut down the total number of candidate pairs evaluated,
@@ -8,23 +9,30 @@ require(parallel)
 #' @param A an m by n real-valued dense or sparse matrix
 #' @param t a threshold value for correlation
 #' @param p projected subspace dimension
+#' @param cores number of CPU cores to use in explicitly parallel code sections
+#'  (note that available BLAS libraries may implicitly use multiple threads)
+#' @param additional arguments passed to \code{\link{irlba}}
 #'
 #' @return A list with three elements:
 #' \enumerate{
-#'   \item \code{cor} A three-column matrix such that the  first two columns contain
+#'   \item \code{indices} A three-column matrix such that the  first two columns contain
 #'         indices of vectors meeting the correlation threshold \code{t},
 #'         and the third column contains the corresponding correlation value.
 #'   \item \code{longest_run} The largest number of successive entries in the
 #'     ordered first singular vector within a projected distance defined by the
 #'     correlation threshold.
-#'   \item \code{candidates} The total number of _possible_ vectors that meet
-#'     the correlation thrshold identified by the algorithm.
+#'   \item \code{n} The total number of _possible_ vectors that meet
+#'     the correlation threshold identified by the algorithm.
+#'   \item \code{total_time} Total run time.
 #' }
-tcor = function(A, t=0.99, p=5)
+tcor = function(A, t=0.99, p=5, cores=detectCores(), ...)
 {
+  t0 = proc.time()
   mu = colMeans(A)
-  s  = sqrt(apply(A, 2, crossprod) - nrow(A) * mu ^ 2) # column norms of centered matrix
-  L  = irlba(A, p, center=mu, scale=s)
+  s  = sqrt(apply(A, 2, crossprod) - nrow(A) * mu ^ 2) # col norms of centered matrix
+  if(any(s < 10*.Machine$double.eps)) stop("the standard deviation is zero for some columns")
+  L  = irlba(A, p, center=mu, scale=s, ...)
+  t1 = (proc.time() - t0)[[3]]
   P  = order(L$v[, 1])  # order the entries of v1 (the permutation in the paper)
   limit = sqrt(2 * (1 - t)) / L$d[1]
 
@@ -46,14 +54,23 @@ tcor = function(A, t=0.99, p=5)
 # This is the big union in step 4 of algorithm 2.1, combined with step 6 to
 # convert back to original indices, and step 7 to evaluate the candiadtes.
 # Each step from 1 to \ell is independent of the others; the steps can run
-# in parallel:
-  indices = mclapply(1:ell, function(i)
+# in parallel, for which we use foreach with an R socket cluster.
+  combine = function(x, y)
+  {
+    list(idx=rbind(x$idx, y$idx), n=x$n + y$n)
+  }
+  cl = makePSOCKcluster(cores)
+  registerDoParallel(cl)
+  on.exit(stopCluster(cl))
+
+  indices = foreach(i=1:ell, .combine=combine, .inorder=FALSE) %dopar%
   {
     d = diff(L$v[P, 1:p, drop=FALSE], lag=i) ^ 2 %*% L$d[1:p] ^ 2
-    j = which(d <= 2 * (1 - t))  # These ordered indices meet the threshold
+    # These ordered indices meet the projected threshold:
+    j = which(d <= 2 * (1 - t))
     n = length(j)
-    # return original un-permuted column indices that meet threshold (step 7)
-    # including the number of possible candidates
+    # return original un-permuted column indices that meet true threshold
+    # (step 7), including the number of possible candidates for info.:
     if(n == 0)
     {
       ans = vector("list", 2)
@@ -66,7 +83,6 @@ tcor = function(A, t=0.99, p=5)
     j = j[h]
     v = v[h]
     return(list(idx=cbind(i=P[j], j=P[j + i], cor=v), n=n))
-  })
-  list(cor=Reduce(rbind, Map(function(x) x$idx, indices)), longest_run=ell,
-         candidates=Reduce(sum, Map(function(x) x$n, indices)))
+  }
+  c(indices, longest_run=ell, irlb_time=t1, total_time=(proc.time() - t0)[[3]])
 }
