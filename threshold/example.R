829765b4eca3874c0263387a8cc306710e173e7e
B. W. Lewis <blewis@illposed.net>
2015-11-27 20:25:59 -0500

    example.R

--- threshold/example.R
+++ threshold/example.R
@@ -1,25 +1,33 @@
-# EXAMPLE USE of tcor
 source("tcor.R")
 
 library(biclust)  # for the EisenYeast and BicatYeast data
 data(BicatYeast)  # in gene by sample orientation (a tiny example for testing)
 A = t(BicatYeast) # to correlate genes to genes
-#data(EisenYeast)  # slightly larger example
-#A = t(EisenYeast) # to correlate genes to genes
+threshold = 0.95
 
-t = 0.95
 
-t1 = proc.time()
-C = cor(A)                     # actual correlation matrix for reference
-C[lower.tri(C,diag=TRUE)] = 0  # cut out symmetric part and ones on diagonal
-C = C[C>t]
-print(proc.time() - t1)
+# Compute using tcor():
+m1  = sum(gc()[,2])  # Memory in megabytes currently in use
+t1  = proc.time()
+tx  = tcor(A, t=threshold, p=10)
+t1  = (proc.time() - t1)[3] # Time in seconds
+m1  = sum(gc()[,6]) - m1    # Peak excess memory use during the test
 
-t1 = proc.time()
-# Compute candidate pairs with the new algorithm
-x = tcor(A, t=t, p=10)
-print(proc.time() - t1)
+cat("tcor time (s)                      ", t1, "\n")
+cat("tcor peak excess memory (MB)       ", m1, "\n")
 
-# Compare
-print(sum(C > t))
-print(nrow(x$cor))
+# Compute using R's native cor() function:
+m2  = sum(gc()[,2])
+t2  = proc.time()
+cx  = cor(A)                # full correlation matrix
+diag(cx) = -Inf
+pairs = which(cx >= threshold, arr.ind=TRUE)
+t2  = (proc.time() - t2)[3]
+m2  = sum(gc()[,6]) - m2
+
+cat("brute force time (s)               ", t2, "\n")
+cat("brute force peak excess memory (MB)", m2, "\n")
+
+# Compare results!
+cat("Number of vector pairs meeting threshold found by brute force", nrow(pairs)/2, "\n")
+cat("Number of vector pairs meeting threshold found by tcor       ", nrow(tx[[1]]), "\n")
