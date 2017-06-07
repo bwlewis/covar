78b026b8dca5e34d689393d0928c6ededa4562af
bwlewis <blewis@illposed.net>
2017-06-07 07:42:59 -0400

    extra correlation codes

---
+++ pearson.R
@@ -0,0 +1,20 @@
+# Example efficient Pearson's sample correlation matrix computation
+#
+# You should have R linked against a high-performance BLAS library for best
+# results. See XXX
+
+#' @param x input m by n data matrix
+#' @result n x n symmetric Pearson's sample correlation matrix
+#' @notes compare with cor(x, method="pearson")
+pearson = function(x)
+{
+  m = nrow(x)
+  z = .colMeans(x, nrow(x), ncol(x))
+  w = apply(x, 2, crossprod)
+  W = 1 / sqrt(w - m * z * z)
+  e = rep(1.0, m)
+  r = (2 * z) %*% crossprod(e, x)
+  r = m * tcrossprod(z) - r
+  ans = crossprod(x) + r
+  sweep((W * ans), 2, W, '*')
+}
